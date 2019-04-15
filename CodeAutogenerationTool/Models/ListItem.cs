81da845216808ef46e3177df0e5c01225a6afb89
Robinson_Luo <Robinson_Luo@users.noreply.github.com>
2019-04-15 09:04:30 +0800

    新增
    新增项目

---
+++ CodeAutogenerationTool/Models/ListItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAutogenerationTool.Models
+{
+    class ListItem
+    {
+        public string Text { get; set; }
+
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 返回ListItem中的Text值
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.Text;
+        }
+    }
+}
